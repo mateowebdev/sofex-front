8f3dc27fc26201684d28092ab1ec3993d3347943
mateowebdev <mateocernaz01@gmail.com>
2023-07-28 20:39:28 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Introo</h1>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
